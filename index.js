33c0cc76c8682cc59d77a59046f0cd9596be16b2
Max Zakzook <mzakzook@me.com>
2019-10-28 14:09:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "is the champion"
+
